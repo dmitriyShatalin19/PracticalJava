c12c16fda2458aa3a669e774b312d2300bcedd4f
Дмитрий <161758737+dmitriyShatalin19@users.noreply.github.com>
2025-03-06 13:46:15 +0300

    Update main2.java

--- main2.java
+++ main2.java
@@ -1,1 +1,19 @@
+import java.util.*;
 
+public class Main {
+  public static void main(String[] args) {
+    Object[] arr = "Hello".split("");
+    shuffle(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+  
+  static void shuffle(Object[] arr) {
+    Random rnd = new Random();
+    for (int i = 0; i < arr.length - 1; i++) {
+      int j = rnd.nextInt(arr.length);
+      Object temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+}
