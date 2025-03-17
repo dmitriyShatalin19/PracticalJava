18e6582a517c427b57312ab63f0a23f75e6eba9d
Дмитрий <161758737+dmitriyShatalin19@users.noreply.github.com>
2025-03-17 22:18:09 +0300

    Update CryptoGenerator.java

--- CryptoGenerator.java
+++ CryptoGenerator.java
@@ -8,26 +8,36 @@ public class CryptoGenerator {
 
 class Symbol {
     char character;
+    int index;
     boolean isVisible;
 
-    Symbol(char character, boolean isVisible) {
+    Symbol(char character, int index, boolean isVisible) {
         this.character = character;
+        this.index = index;
         this.isVisible = isVisible;
     }
 
     public String toString() {
         return isVisible ? String.valueOf(character) : "_";
     }
+
+    public String getIndexString() {
+        return isVisible ? " " : String.valueOf(index);
+    }
 }
 
 class Word {
     List<Symbol> symbols;
 
-    Word(String word, Random random) {
+    Word(String word, int startIndex, Random random) {
         symbols = new ArrayList<>();
+        int index = startIndex;
         for (char c : word.toCharArray()) {
-            boolean isVisible = Character.isWhitespace(c) || !Character.isLetter(c) || random.nextBoolean();
-            symbols.add(new Symbol(c, isVisible));
+            boolean isVisible = !Character.isLetter(c) || random.nextBoolean();
+            symbols.add(new Symbol(c, isVisible ? -1 : index, isVisible));
+            if (Character.isLetter(c)) {
+                index++;
+            }
         }
     }
 
@@ -35,6 +45,14 @@ class Word {
         StringBuilder sb = new StringBuilder();
         for (Symbol s : symbols) {
             sb.append(s).append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    public String getIndexString() {
+        StringBuilder sb = new StringBuilder();
+        for (Symbol s : symbols) {
+            sb.append(s.getIndexString()).append(" ");
         }
         return sb.toString().trim();
     }
@@ -58,10 +76,25 @@ class Generator {
     void generate() {
         String quote = quotes.get(random.nextInt(quotes.size()));
         String[] words = quote.split(" ");
+        List<Word> wordObjects = new ArrayList<>();
+        int letterIndex = 1;
         
         for (String word : words) {
-            Word w = new Word(word, random);
-            System.out.print(w.getWordString() + " ");
+            Word w = new Word(word, letterIndex, random);
+            wordObjects.add(w);
+            for (char c : word.toCharArray()) {
+                if (Character.isLetter(c)) {
+                    letterIndex++;
+                }
+            }
+        }
+        
+        for (Word word : wordObjects) {
+            System.out.print(word.getWordString() + " ");
+        }
+        System.out.println();
+        for (Word word : wordObjects) {
+            System.out.print(word.getIndexString() + " ");
         }
         System.out.println();
     }
